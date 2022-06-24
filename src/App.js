2e217396859051927c88514c42cfd0ca865eb82a
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-24 09:14:02 +0300

    first example of passing props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
       <h2>App.js</h2>
-      <CustomComponent/>
+      <CustomComponent  title="This string passed through props from App.js"/>
     </div>
   );
 }
--- src/components/CustomComponent.jsx
+++ src/components/CustomComponent.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './CustomComponent.css'
 
-function CustomComponent() {
+function CustomComponent(props) {
   return (
     <div className='custom-component'>
-        <h3>CustomComponent.jsx</h3>
+        <h3>CustomComponent.jsx </h3>
+        <p> {props.title}</p>
     </div>
   )
 }
